5da53bb9efd223a09e3a03f7e15c1cafaa79cc13
Suresh <suris6530@gmail.com>
2026-02-25 11:49:58 +0530

    duplicates deleted

--- girijakalyana.c/android/app/src/main/java/com/girijakalyana/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.girijakalyana.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- girijakalyana.c/src/components/usecontext/ProfileContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const ProfileContext = createContext({ 
-  profile: null, 
-  getProfileByRegistrationNo: async () => null,
-  setProfile: () => {}
-});
-
-export default ProfileContext;
